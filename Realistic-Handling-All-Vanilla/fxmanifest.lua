6228ea1d0e8d0a958f8bb6fbdcf25876af8e9ad5
dsvipeer <112516086+dsvipeer@users.noreply.github.com>
2023-01-24 08:52:14 -0300

    Add files via upload

---
+++ Realistic-Handling-All-Vanilla/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name "Realistic handling for all Vanilla cars."
+description "A handling.meta with 18.858 lines of code to give you a better experience driving vanilla cars."
+
+files {
+	'data/**/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
